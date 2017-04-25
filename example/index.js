cf395f9ff26e09814f413706b3dc4969c76e801e
Sunil Pai <threepointone@gmail.com>
2017-04-26 00:03:39 +0530

    readme

--- example/index.js
+++ example/index.js
@@ -1,19 +1,19 @@
-// import React from 'react'
-// import { render } from 'react-dom'
+import React from 'react'
+import { render } from 'react-dom'
 import css from '../src'
 
-/* whowhowho */
 let blue = 'blue'
+
 let cls = css`font-weight:bold; color: ${blue}`
 
 let cls2 = css`color: green;`
 
-// class App extends React.Component{
-//   render(){
-//     return <div className={cls}>
-//       what up
-//     </div>
-//   }
-// }
+class App extends React.Component{
+  render(){
+    return <div className={cls}>
+      what up
+    </div>
+  }
+}
 
-// render(<App/>, window.root)
+render(<App/>, window.root)
